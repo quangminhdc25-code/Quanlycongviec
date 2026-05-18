770effe39e415c96df684e543ad081c75c6e0382
quangminhdc25-code <quangminhdc25@gmail.com>
2026-05-18 09:20:49 +0000

    Đổi tên tab

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="vi">
       <head>
-        <title>Quản lý công việc Xã Gào</title>
+        <title>Quản lý công việc ICT Gia Lai Tây</title>
       </head>
       {/* Lệnh m-0 p-0 overflow-hidden giúp web tràn sát viền */}
       <body className="m-0 p-0 overflow-hidden bg-black">
